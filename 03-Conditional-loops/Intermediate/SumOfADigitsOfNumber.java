c7a0df84d9955dd40a514978e125033c1346b4df
rupesh-darimisetti <rupeshdarimisetti@gmail.com>
2022-08-29 22:10:48 +0530

    add 19, 20, 22, 23, 24, 25, 26 solution of intermediate conditional loops

---
+++ 03-Conditional-loops/Intermediate/SumOfADigitsOfNumber.java
@@ -0,0 +1,19 @@
+package Intermediate;
+
+import java.util.Scanner;
+
+// 24. Sum Of A Digits Of Number
+public class SumOfADigitsOfNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to find Sum Of A Digits Of Number: ");
+        long num = input.nextLong();
+        long sum = 0, rem, real_num = num;
+        while(num != 0){
+            rem = num % 10;
+            sum += rem;
+            num /= 10;
+        }
+        System.out.println("The sum of individual digits of " + real_num + " is " + sum);
+    }
+}
