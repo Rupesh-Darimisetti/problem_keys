f534e549036411e2d5b90015b605bb1743adeecf
rupesh-darimisetti <rupeshdarimisetti@gmail.com>
2022-08-30 22:34:18 +0530

    add 5, 10, 18 and complete solutions of conditionl loops

---
+++ 03-Conditional-loops/Intermediate/DistanceBetweenTwoPoints.java
@@ -0,0 +1,19 @@
+package Intermediate;
+
+import java.util.Scanner;
+// Calculate Distance Between Two Points
+public class DistanceBetweenTwoPoints {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter x1 :");
+        float x1 = input.nextFloat();
+        System.out.print("Enter y1: ");
+        float y1 = input.nextFloat();
+        System.out.print("Enter x2: ");
+        float x2 = input.nextFloat();
+        System.out.print("Enter y2: ");
+        float y2 = input.nextFloat();
+        double distance_between_two_points = Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1) , 2));
+        System.out.println("The Distance between two points = " + distance_between_two_points);
+    }
+}
