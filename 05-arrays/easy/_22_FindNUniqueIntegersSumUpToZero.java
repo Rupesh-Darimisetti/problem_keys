ae625b18ed469f7729d54f7a649fc99fe8f44163
rupesh-darimisetti <rupeshdarimisetti@gmail.com>
2022-09-23 20:22:17 +0530

    add 22 easy array sum to find n unique integers sum up to zero

---
+++ 05-arrays/easy/_22_FindNUniqueIntegersSumUpToZero.java
@@ -0,0 +1,33 @@
+package easy;
+
+import java.util.Arrays;
+
+public class _22_FindNUniqueIntegersSumUpToZero {
+    public static void main(String[] args) {
+        int  n = 5;
+        int[] ans = sumZero(n);
+        System.out.println(Arrays.toString(ans));
+    }
+    private static int[] sumZero(int n) {
+        int[] arr = new int[n];
+        int val;
+
+//        if (n % 2 == 0) {
+            val = -n + 1;
+            int index = 0;
+            while (index < n) {
+                arr[index] = val;
+                val += 2;
+                index++;
+            }
+//        } else {
+//            val = -n / 2;
+//            int index = 0;
+//            while (index < n) {
+//                arr[index] = val++;
+//                index++;
+//            }
+//        }
+        return arr;
+    }
+}
