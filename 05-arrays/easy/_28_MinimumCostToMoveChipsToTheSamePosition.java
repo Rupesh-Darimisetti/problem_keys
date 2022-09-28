f677203a3b62a3fc1fddac8c7eb87c162721ea76
rupesh-darimisetti <rupeshdarimisetti@gmail.com>
2022-09-28 20:44:58 +0530

    add 28th array easy solution

---
+++ 05-arrays/easy/_28_MinimumCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class _28_MinimumCostToMoveChipsToTheSamePosition {
+    public static void main(String[] args) {
+        int[] position = {2,2,2,3,3};
+        int cost = minCostToMoveChips(position);
+        System.out.println(cost);
+    }
+    private static int minCostToMoveChips(int[] position) {
+        int count1 = 0,count2 = 0;
+        for (int j : position) {
+            if (j % 2 != 0) count1++;
+            else count2++;
+        }
+        return Math.min(count1, count2);
+    }
+}
