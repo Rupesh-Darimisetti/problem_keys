fe1a161c09c75d932a9ebcc645a3f071cfadf1a6
rupesh-darimisetti <rupeshdarimisetti@gmail.com>
2022-10-11 22:08:57 +0530

    add arrays medium 9th solution

---
+++ 05-arrays/medium/_9_SortColors.java
@@ -0,0 +1,39 @@
+package medium;
+
+public class _9_SortColors {
+    public static void main(String[] args) {
+
+    }
+    public void sortColors(int[] nums) {
+        int begin = 0;
+        int end = nums.length - 1;
+
+        while (begin < end){
+            if (nums[begin] == 0)  begin++;
+            if (nums[end] != 0)  end--;
+            if (nums[begin] != 0 && nums[end] == 0 && begin < end){
+                int num = nums[begin];
+                nums[begin] = nums[end];
+                nums[end] = num;
+                begin ++;
+                end--;
+            }
+        }
+
+        end = nums.length - 1;
+
+        if (nums[begin] == 0)  begin++;
+
+        while (begin < end){
+            if (nums[begin] == 1)  begin ++;
+            if (nums[end] > 1)  end--;
+            if (nums[begin] > 1 && nums[end] == 1 && begin < end){
+                int num = nums[begin];
+                nums[begin] = nums[end];
+                nums[end] = num;
+                begin ++;
+                end--;
+            }
+        }
+    }
+}
