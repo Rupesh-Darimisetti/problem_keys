572f6910b18d4b4ec75a0c929d151f207daf2ee3
rupesh-darimisetti <rupeshdarimisetti@gmail.com>
2022-11-02 21:16:49 +0530

    add two sum II input array is sorted solution of 6 searching easy

---
+++ 06-searching/easy/_04_TwoSumII_InputArrayIsSorted.java
@@ -0,0 +1,28 @@
+package easy;
+
+import java.util.Arrays;
+
+public class _04_TwoSumII_InputArrayIsSorted {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{-1, 0};
+        int target = -1;
+        int[] res = twoSum(numbers, target);
+        System.out.println(Arrays.toString(res));
+    }
+    private static int[] twoSum(int[] numbers, int target) {
+        int sum = 0;
+        int[] ans = {0,0};
+        int first = 0; int last = numbers.length -1;
+        while(true){
+            sum = numbers[first] + numbers[last];
+            if(sum == target){
+                ans[0] = first +1;
+                ans[1] = last +1;
+                break;
+            } else if(sum > target){
+                last--;
+            } else first++;
+        }
+        return ans;
+    }
+}
