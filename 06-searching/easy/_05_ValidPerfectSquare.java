7be6bbb65827c25d7fad848d80348c57aeb4060e
rupesh-darimisetti <rupeshdarimisetti@gmail.com>
2022-11-04 22:37:27 +0530

    add solution to valid perfect square of searching

---
+++ 06-searching/easy/_05_ValidPerfectSquare.java
@@ -0,0 +1,27 @@
+package easy;
+
+public class _05_ValidPerfectSquare {
+    public static void main(String[] args) {
+        int number = 16;
+        boolean validPerfectSquare = isPerfectSquare(number);
+        System.out.println(validPerfectSquare);
+    }
+
+    private static boolean isPerfectSquare(int num){
+        long low =1;
+        long high = num;
+        while(low <= high){
+            long mid = low + (high - low) / 2;
+            if(mid * mid == num){
+                return true;
+            }
+            else if(mid * mid > num) {
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
