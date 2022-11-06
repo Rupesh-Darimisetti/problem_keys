c3afbf9672b386029e110bb7b0dcaf927f835c4e
rupesh-darimisetti <rupeshdarimisetti@gmail.com>
2022-11-06 19:35:40 +0530

    add next greatest letter solution of 06 searching

---
+++ 06-searching/easy/_07_FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,25 @@
+package easy;
+
+public class _07_FindSmallestLetterGreaterThanTarget {
+    public static void main(String[] args) {
+        char[] letters = {'c','f','j'};
+        char target = 'c';
+        char greaterAlphabetThanTarget = nextGreatestLetter(letters, target);
+        System.out.println(greaterAlphabetThanTarget);
+    }
+    private static char nextGreatestLetter(char[] letters, char target){
+
+        int start = 0, end = letters.length;
+
+        while(start < end){
+            int mid = start +(end - start) / 2;
+            if(letters[mid] > target){
+                end = mid;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
