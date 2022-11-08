e9d64ee0c6204ac3ac5cdef54b1a184c34e1cf60
rupesh-darimisetti <rupeshdarimisetti@gmail.com>
2022-11-08 21:22:17 +0530

    Add Search Insert Position solution of 06 searching easy

---
+++ 06-searching/easy/_09_SearchInsertPosition.java
@@ -0,0 +1,30 @@
+package easy;
+
+/**
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ */
+public class _09_SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] number = {1,3,5,6};
+        int target = 7;
+        int searchInsertResult = searchInsert(number, target);
+        System.out.println(searchInsertResult);
+    }
+    private static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] > target) {
+                end = mid - 1;
+            } else if(nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
